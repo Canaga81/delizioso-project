80a01e02379e32664d70d99686c98c19210c86ae
Canaga81 <Canagamemmedov@gmail.com>
2024-02-07 13:39:24 +0400

    Customers Func Finish

--- src/Components/Customers/Customers.jsx
+++ src/Components/Customers/Customers.jsx
@@ -1,118 +1,50 @@
-// import { useState } from "react";
-// import Customer_1_image from "../../Images/Customer_1.png";
-// import Customer_2_image from "../../Images/Customer_2.png";
-// import Customer_3_image from "../../Images/Customer_3.png";
-// import Customer_4_image from "../../Images/Customer_4.png";
-// import Customer_5_image from "../../Images/Customer_5.png";
-// import Customer_6_image from "../../Images/Customer_6.png";
-// import Customer_7_image from "../../Images/Customer_7.png";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { EffectCoverflow, Autoplay } from "swiper/modules";
-
-import "./style.css";
+import { useState } from "react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+import imagesTextData from "../../ImagesTextData";
 
 const Customers = () => {
-  // const [index, setIndex] = useState(0);
-
-  // const img = [
-  //   Customer_1_image,
-  //   Customer_2_image,
-  //   Customer_3_image,
-  //   Customer_4_image,
-  //   Customer_5_image,
-  //   Customer_6_image,
-  //   Customer_7_image,
-  // ];
+  const [selectedImg, setSelectedImg] = useState(imagesTextData[0]);
 
   return (
-    <div className="w-full h-full mb-60">
+    <div className="w-full h-full pt-52 pb-52 mb-60 bg-[#D0CCC7] bg-opacity-20">
       <div className="container mx-auto">
         <div className="flex flex-col gap-14 items-center">
           <div className="text-[#311F09] font-primary lg:text-[60px] text-[35px] font-normal text-center">
             Our customers say
           </div>
-          {/* <div>
-            <div className="slider-content">
-              {img.map((item, i) => {
-                index === i && (
-                  <img
-                    src={item}
-                    className="w-[200px] h-[200px] rounded-full"
-                    alt=""
-                  />
+          <div className="flex flex-col gap-24">
+            <div className="flex flex-col items-center gap-16">
+              <img
+                className="sm:w-[200px] sm:h-[200px] w-[160px] h-[160px] cursor-pointer"
+                src={selectedImg}
+                alt="Selected"
+              />
+              <p className="max-w-[500px] text-center font-primary lg:text-[20px] md:text-[18px] sm:text-[16px] text-[14px] text-[#5C4529] tracking-[1.5px]">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Facilisis ultricies at eleifend proin. Congue nibh nulla
+                malesuada ultricies nec quam
+              </p>
+            </div>
+            <div className="flex justify-center gap-6 flex-wrap items-center">
+              {imagesTextData.map((item, idx) => {
+                return (
+                  <div
+                    className="sm:w-[150px] sm:h-[150px] w-[120px] h-[120px]"
+                    key={idx}
+                  >
+                    <img
+                      className="w-full h-full cursor-pointer object-contain hover:bg-[#FF8A00] hover:scale-105 rounded-full p-3 transition duration-200"
+                      src={item.image}
+                      alt="images"
+                      onClick={() => setSelectedImg(item.image)}
+                    />
+                  </div>
                 );
               })}
             </div>
-            <div className="swiper-content">
-              <Swiper
-                loop={true}
-                modules={[EffectCoverflow, Autoplay]}
-                // autoplay={{ delay: 1000 }}
-                effect={"coverflow"}
-                coverflowEffect={{
-                  rotate: 0,
-                  stretch: 10,
-                  depth: -30,
-                  modifier: 1,
-                  slideShadows: false,
-                }}
-              >
-                <SwiperSlide>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Facilisis ultricies at eleifend proin. Congue nibh nulla
-                    malesuada ultricies nec quam
-                  </p>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Facilisis ultricies at eleifend proin. Congue nibh nulla
-                    malesuada ultricies nec quam
-                  </p>
-                </SwiperSlide>
-              </Swiper>
-            </div>
-            <div className="justyfy">
-              <Swiper
-                loop={true}
-                effect={"coverflow"}
-                initialSlide={0}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={5}
-                //  spaceBetween={20}
-                onSlideChange={(e) => {
-                  setIndex(e.realIndex);
-                }}
-                coverflowEffect={{
-                  rotate: 0,
-                  stretch: 10,
-                  depth: -30,
-                  modifier: 1,
-                  scale: 0.8,
-                }}
-                pagination={true}
-                modules={[EffectCoverflow, Autoplay]}
-                autoplay={{ delay: 1000 }}
-                className="mySwiper"
-              >
-                {img.map((item, index) => (
-                  <SwiperSlide key={index}>
-                    <img
-                      className="slider-img"
-                      style={{ height: "150px" }}
-                      src={item}
-                      alt="json"
-                    />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/ImagesTextData.jsx
@@ -0,0 +1,51 @@
+//! Import Images
+import Customer_1_Image from './Images/Customer_1.png';
+import Customer_2_Image from './Images/Customer_2.png';
+import Customer_3_Image from './Images/Customer_3.png';
+import Customer_4_Image from './Images/Customer_4.png';
+import Customer_5_Image from './Images/Customer_5.png';
+import Customer_6_Image from './Images/Customer_6.png';
+import Customer_7_Image from './Images/Customer_7.png';
+
+
+const imagesTextData = [
+
+    {
+        id: 1,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias nulla.",
+        image: Customer_1_Image
+    },
+    {
+        id: 2,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam !",
+        image: Customer_2_Image
+    },
+    {
+        id: 3,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias nulla, culpa impedit.",
+        image: Customer_3_Image
+    },
+    {
+        id: 4,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        image: Customer_4_Image
+    },
+    {
+        id: 5,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias nulla, culpa impedit.",
+        image: Customer_5_Image
+    },
+    {
+        id: 6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias nulla !",
+        image: Customer_6_Image
+    },
+    {
+        id: 7,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias !",
+        image: Customer_7_Image
+    },
+
+]
+
+export default imagesTextData;
